6095b1f5c68be1fd10c7157660760cbc706eccb8
Danil Vasilchikov <DaVas255@gmail.com>
2026-02-24 20:11:52 +0700

    refactor: remove api-key, error, and tracked-site modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common'
-import { ErrorModule } from './error/error.module'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, ErrorModule]
+	imports: [ConfigModule.forRoot(), AuthModule]
 })
 export class AppModule {}
